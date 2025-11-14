c3873fb047a87771fa9660fd3bf3e11ac30d9fb9
azamat-9933 <azamat.yakhyayeff@gmail.com>
2025-11-15 01:29:04 +0500

    created bot structure

---
+++ run.py
@@ -0,0 +1,25 @@
+import asyncio
+from aiogram import Bot
+from aiogram import Dispatcher
+from aiogram.client.default import DefaultBotProperties
+from aiogram.enums import ParseMode
+
+from configs import TOKEN
+from handlers.user_handlers import user_router
+
+
+
+bot = Bot(TOKEN, default=DefaultBotProperties(parse_mode=ParseMode.HTML))
+
+dp = Dispatcher()
+
+dp.include_routers(user_router)
+
+
+async def main():
+    await bot.delete_webhook(drop_pending_updates=True)
+    await dp.start_polling(bot)
+
+
+if __name__ == '__main__':
+    asyncio.run(main())
